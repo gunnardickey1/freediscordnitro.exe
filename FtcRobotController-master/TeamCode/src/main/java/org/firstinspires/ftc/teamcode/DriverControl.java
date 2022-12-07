00f3682505154bc912b4ba8149a3b5d1b1429a0c
gunnardickey1 <56365361+gunnardickey1@users.noreply.github.com>
2022-12-07 16:04:38 -0500

    change teleop

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -5,16 +5,17 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "DriverControl", group = "Main")
+@TeleOp(name = "schizophrenic_ramblings", group = "Main")
 // @Disabled
 public class DriverControl extends OpMode {
     private DcMotor frontLeft;
     private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+    private DcMotor rearLeft;
+    private DcMotor rearRight;
     private DcMotor arm;
     private Servo claw;
-    boolean open = false;
+    private Servo initclaw;
+    boolean open = true;
     boolean slow = false;
     // private DcMotor magMotor;
     // private DcMotor spinnerMotor;
@@ -23,12 +24,16 @@ public class DriverControl extends OpMode {
     public void init() {
         frontLeft = hardwareMap.dcMotor.get("frontLeft");
         frontRight = hardwareMap.dcMotor.get("frontRight");
-        backLeft = hardwareMap.dcMotor.get("backLeft");
-        backRight = hardwareMap.dcMotor.get("backRight");
+        rearLeft = hardwareMap.dcMotor.get("rearLeft");
+        rearRight = hardwareMap.dcMotor.get("rearRight");
         arm = hardwareMap.dcMotor.get("arm");
         claw = hardwareMap.servo.get("claw");
+        initclaw = hardwareMap.servo.get("initClaw");
         // magMotor = hardwareMap.dcMotor.get("magMotor");
         // spinnerMotor = hardwareMap.dcMotor.get("spinnerMotor");
+        claw.setPosition(0.5);
+        initclaw.setPosition(0.5);
+
 
     }
 
@@ -52,55 +57,55 @@ public class DriverControl extends OpMode {
             if (!slow) {
                 frontLeft.setPower(y);
                 frontRight.setPower(-y);
-                backLeft.setPower(y);
-                backRight.setPower(-y);
+                rearLeft.setPower(y);
+                rearRight.setPower(-y);
             } else {
                 frontLeft.setPower(y / 2);
                 frontRight.setPower(-y / 2);
-                backLeft.setPower(y / 2);
-                backRight.setPower(-y / 2);
+                rearLeft.setPower(y / 2);
+                rearRight.setPower(-y / 2);
             }
         } else {
             frontLeft.setPower(0);
             frontRight.setPower(0);
-            backLeft.setPower(0);
-            backRight.setPower(0);
+            rearLeft.setPower(0);
+            rearRight.setPower(0);
         }
         if ((x > 0.1 || x < -0.1) && (y < 0.15 && y > -0.15)) {
             if (!slow) {
                 frontLeft.setPower(x);
                 frontRight.setPower(x);
-                backLeft.setPower(-x);
-                backRight.setPower(-x);
+                rearLeft.setPower(-x);
+                rearRight.setPower(-x);
             } else {
                 frontLeft.setPower(x / 2);
                 frontRight.setPower(x / 2);
-                backLeft.setPower(-x / 2);
-                backRight.setPower(-x / 2);
+                rearLeft.setPower(-x / 2);
+                rearRight.setPower(-x / 2);
             }
         } else {
             frontLeft.setPower(0);
             frontRight.setPower(0);
-            backLeft.setPower(0);
-            backRight.setPower(0);
+            rearLeft.setPower(0);
+            rearRight.setPower(0);
         }
         if (gamepad1.right_stick_x > 0.1 || gamepad1.right_stick_x < -0.1) {
             if (!slow) {
                 frontLeft.setPower(-z);
                 frontRight.setPower(-z);
-                backLeft.setPower(-z);
-                backRight.setPower(-z);
+                rearLeft.setPower(-z);
+                rearRight.setPower(-z);
             } else {
                 frontLeft.setPower(-z / 2);
                 frontRight.setPower(-z / 2);
-                backLeft.setPower(z / 2);
-                backRight.setPower(-z / 2);
+                rearLeft.setPower(z / 2);
+                rearRight.setPower(-z / 2);
             }
         } else {
             frontLeft.setPower(0);
             frontRight.setPower(0);
-            backLeft.setPower(0);
-            backRight.setPower(0);
+            rearLeft.setPower(0);
+            rearRight.setPower(0);
         }
         if (gamepad1.right_trigger == 1.0) {
             // magMotor.setPower(-1);
@@ -126,8 +131,10 @@ public class DriverControl extends OpMode {
             // spinnerMotor.setPower(0);
         }
         if (gamepad1.dpad_up) {
-            arm.setPower(1);
-        } else if (gamepad1.dpad_down) {
+            arm.setPower(-1);
+
+        }
+        else if (gamepad1.dpad_down) {
             arm.setPower(-1);
         } else {
             arm.setPower(0);
